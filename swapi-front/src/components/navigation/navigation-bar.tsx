4a26c59dff2159f1eeac332c4b5dd5254ac8a101
Maxime Dauvergne <dauvergne.maxime@gmail.com>
2019-03-02 11:26:48 +0100

    Adding top navigation bar

---
+++ swapi-front/src/components/navigation/navigation-bar.tsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { Avatar } from '@material-ui/core';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+
+const styles = createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    purpleAvatar: {
+        margin: 10,
+        color: '#fff',
+        backgroundColor: deepPurple[500],
+      },
+  });
+  
+  export interface Props extends WithStyles<typeof styles> {}
+  
+  export interface State {
+    auth: boolean;
+    anchorEl: null | HTMLElement;
+  }
+  
+  class MenuAppBar extends React.Component<Props, State> {
+    state: State = {
+      auth: true,
+      anchorEl: null,
+    };
+  
+    handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+      this.setState({ anchorEl: event.currentTarget });
+    };
+  
+    handleClose = () => {
+      this.setState({ anchorEl: null });
+    };
+  
+    render() {
+      const { classes } = this.props;
+      const { auth, anchorEl } = this.state;
+      const open = Boolean(anchorEl);
+  
+      return (
+        <div className={classes.root}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" className={classes.grow}>
+                Star Wars Planets
+              </Typography>
+              {auth && (
+                <div>
+                  <IconButton
+                    aria-owns={open ? 'menu-appbar' : undefined}
+                    aria-haspopup="true"
+                    onClick={this.handleMenu}
+                    color="inherit"
+                  >
+                   <Avatar className={classes.purpleAvatar}>MD</Avatar>
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorEl}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'right',
+                    }}
+                    open={open}
+                    onClose={this.handleClose}
+                  >
+                    <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                    <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                  </Menu>
+                </div>
+              )}
+            </Toolbar>
+          </AppBar>
+        </div>
+      );
+    }
+  }
+  
+  (MenuAppBar as React.ComponentClass<Props>).propTypes = {
+    classes: PropTypes.object.isRequired,
+  } as any;
+  
+  export default withStyles(styles)(MenuAppBar);
