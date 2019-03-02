80775ff4e4dd970b3500f7b787b909b4eff79d90
Maxime Dauvergne <dauvergne.maxime@gmail.com>
2019-03-02 11:30:29 +0100

    Adding planetSearch component

---
+++ swapi-front/src/components/planets/planet-search.tsx
@@ -0,0 +1,90 @@
+import React, { Component } from 'react'
+import SearchIcon from '@material-ui/icons/Search';
+import { createStyles, Theme, withStyles, WithStyles } from '@material-ui/core/styles';
+import { InputBase } from '@material-ui/core';
+
+const styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginLeft: -12,
+      marginRight: 20,
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing.unit,
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      width: theme.spacing.unit * 9,
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+      width: '100%',
+    },
+    inputInput: {
+      paddingTop: theme.spacing.unit,
+      paddingRight: theme.spacing.unit,
+      paddingBottom: theme.spacing.unit,
+      paddingLeft: theme.spacing.unit * 10,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: 120,
+        '&:focus': {
+          width: 200,
+        },
+      },
+    },
+  });
+
+export interface Props extends WithStyles<typeof styles> {}
+
+
+class PlanetSearch extends Component<any, any> {
+    render() {
+        const { classes } = this.props;
+
+    return (
+        <div className="PlanetSearch">
+       <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+          </div>
+      </div>  
+      )
+    }
+}
+
+
+export default withStyles(styles)(PlanetSearch);
